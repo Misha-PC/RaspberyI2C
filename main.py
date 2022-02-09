d707e0ad0097737dc947ba4b1ac8b2a8f0a83688
Misha-PC <misha.rus.ru@gmail.com>
2022-02-09 15:14:46 +0300

    print current time

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from lcd import LcdI2C
+import sys
+from datetime import datetime
+from time import sleep
 
+cur_t = ''
 if __name__ == '__main__':
-    import sys
-    lcd2004 = LcdI2C(0x27, 1)
-
-    if len(sys.argv) > 1:
-        lcd2004.display_string(sys.argv[1], 1)
-    else:
-        lcd2004.display_string("Wait string...", 1)
+    while True:
+        lcd2004 = LcdI2C(0x27, 1)
+        time = datetime.now().strftime("%h, %m  %H:%M")
+        if time != cur_t:
+            cur_t = time
+            lcd2004.display_string(time, 1)
+            print(f"\n\tSet: {time}")
+        print(".", end='')
+        sleep(1)
