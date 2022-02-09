8b18d98ebec5d5304065343a4d377c807c331568
Misha-PC <misha.rus.ru@gmail.com>
2022-02-09 15:26:52 +0300

    fix main

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from time import sleep
 
 cur_t = ''
 if __name__ == '__main__':
+    lcd2004 = LcdI2C(0x27, 1)
     while True:
-        lcd2004 = LcdI2C(0x27, 1)
         time = datetime.now().strftime("%h, %m  %H:%M")
         if time != cur_t:
             cur_t = time
+            print(type(time))
             lcd2004.display_string(time, 1)
             print(f"\n\tSet: {time}")
         print(".", end='')
